1fa47eded424454ad7098282b45a0d521878ba54
sagarpatra99 <sagarpatra641@gmail.com>
2026-03-13 11:24:37 +0530

    removed smtp gmail and added  resend

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,39 +1,61 @@
-import nodemailer from "nodemailer";
-// const nodemailer = require("nodemailer");
+// import nodemailer from "nodemailer";
+// // const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  //   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    type: "OAuth2",
-    user: process.env.GOOGLE_USER,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-  },
-  connectionTimeout: 10000,
-});
+// const transporter = nodemailer.createTransport({
+//   //   service: "gmail",
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.GOOGLE_USER,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+//     clientId: process.env.GOOGLE_CLIENT_ID,
+//   },
+//   connectionTimeout: 10000,
+// });
 
-transporter
-  .verify()
-  .then(() => {
-    console.log("Email transporter is ready to send emails");
-  })
-  .catch((err) => {
-    console.log("Email transporter verification failed:", err);
-  });
+// transporter
+//   .verify()
+//   .then(() => {
+//     console.log("Email transporter is ready to send emails");
+//   })
+//   .catch((err) => {
+//     console.log("Email transporter verification failed:", err);
+//   });
 
-export const sendEmail = async ({ to, subject, html, text }) => {
-  const mailOptions = {
-    from: process.env.GOOGLE_USER,
-    to,
-    subject,
-    html,
-    text,
-  };
+// export const sendEmail = async ({ to, subject, html, text }) => {
+//   const mailOptions = {
+//     from: process.env.GOOGLE_USER,
+//     to,
+//     subject,
+//     html,
+//     text,
+//   };
 
-  const details = await transporter.sendMail(mailOptions);
-  console.log(`Email sent:`, details);
+//   const details = await transporter.sendMail(mailOptions);
+//   console.log(`Email sent:`, details);
+// };
+
+
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const response = await resend.emails.send({
+      from: "DO IT <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent:", response);
+
+  } catch (error) {
+    console.error("Email sending failed:", error);
+  }
 };
